cba6be4879509514e86e3d9fa78053b72a8c327e
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2023-01-24 11:19:49 -0300

    atualizando o README

--- 0_PROF/00_mui/src/App.js
+++ 0_PROF/00_mui/src/App.js
@@ -1,60 +1,8 @@
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Signin from "./components/signin/Signin";
 
 function App() {
   return (
-    <Container component="main" maxWidth="xs">
-
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 8
-        }}
-      >
-
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign In
-        </Button>
-      </Box>
-    </Container>
+     <Signin />
   );
 }
 
---
+++ 0_PROF/00_mui/src/components/signin/Signin.jsx
@@ -0,0 +1,77 @@
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+
+function Signin() {
+    return (
+        <Container component="main" maxWidth="xs">
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 8
+                }}
+            >
+
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                />
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                />
+
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Sign In
+                </Button>
+
+                <Grid container>
+                    <Grid item xs>
+                        <Link href="#" underline='none'>
+                            Forgot password?
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link href="#" underline='none'>
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Grid>
+                </Grid>
+
+            </Box>
+        </Container>
+    );
+}
+
+export default Signin;
