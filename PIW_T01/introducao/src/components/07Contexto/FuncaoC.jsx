38e6ae9714f3765b6e0d969f15af7544874e3774
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2023-03-28 21:35:44 -0300

    context

--- PIW_T01/introducao/src/components/07Contexto/FuncaoC.jsx
+++ PIW_T01/introducao/src/components/07Contexto/FuncaoC.jsx
@@ -1,9 +1,12 @@
 import FuncaoD from "./FuncaoD"
+import MinhaCor from "./MeuContexto"
+import { useContext } from "react"
 
 const FuncaoC = () => {
+    const {minhaCor} = useContext(MinhaCor)
     return (
         <div>
-            <h1>Função C</h1>
+            <h1 style={{backgroundColor:minhaCor}}>Função C</h1>
             <FuncaoD/>
         </div>
     )
