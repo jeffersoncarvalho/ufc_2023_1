bd901b86c73c07cae08663e978a676fad1d94402
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2023-04-03 07:37:15 -0300

    Atividade 02

--- WEB_T03/introducao/src/components/06Contexto/FuncaoA.jsx
+++ WEB_T03/introducao/src/components/06Contexto/FuncaoA.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react"
 import FuncaoB from "./FuncaoB"
 import FuncaoC from "./FuncaoC"
 import MinhaCor from "./MeuContexto"
 
 const FuncaoA = () => {
-    const cor = {cor:"red",nome:"Jefferson"}
+    //const cor = {cor:"red",nome:"Jefferson"}
+    const [cor,setCor] = useState({cor:"red",nome:"Jefferson",flag:true})
+    
+    
     return (
         <MinhaCor.Provider value={cor}>
             <div>
                 <h1 style={{ backgroundColor: cor.cor }}>Função A</h1>
                 <FuncaoB />
                 <FuncaoC />
+            </div>
+            <div>
+                <button 
+                    onClick={
+                        () => {
+                            if(cor.flag)
+                                setCor({cor:"green",nome:"Rainara",flag:false})
+                            else setCor({cor:"red",nome:"Jefferson",flag:true})
+                        }
+                    }
+                >
+                    Mudar Cor
+                </button>
             </div>
         </MinhaCor.Provider>
     )
