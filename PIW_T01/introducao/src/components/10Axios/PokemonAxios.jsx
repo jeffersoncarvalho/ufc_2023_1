5dd19e4f65aeb1c404c3ca882da81685adfda4e5
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2023-04-17 14:29:12 -0300

    axios e redux

--- PIW_T01/introducao/src/components/10Axios/PokemonAxios.jsx
+++ PIW_T01/introducao/src/components/10Axios/PokemonAxios.jsx
@@ -4,12 +4,30 @@ import { useState } from "react"
 
 const PokemonAxios = () => {
 
+    const [id,setId] = useState(100)
     const [nome,setNome] = useState("Bulbassaur")
     const [imagem1,setImagem1] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png")
     const [imagem2,setImagem2] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
 
 
-    useEffect
+    useEffect(
+        () => {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(
+                (response)=>{
+                    /*console.log(response.data.name)
+                    console.log(response.data.sprites.back_default)
+                    console.log(response.data.sprites.front_default)*/
+                    setNome(response.data.name)
+                    setImagem1(response.data.sprites.back_default)
+                    setImagem2(response.data.sprites.front_default)
+                }
+            )
+            .catch((error)=>console.log(error))
+        }
+        ,
+        [] //caso vc queira emular um constutor, ou seja, executar apenas uma única vez, não coloque variáveis na lista de variáveis.
+    )
 
     return (
         <>
